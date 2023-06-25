8176cd4ff06dbcf5f6470edc7bf27da79d0a3064
Lazarus <papialazarus@gmail.com>
2023-06-25 14:09:01 +0200

    changes

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -12,11 +12,15 @@ const AboutMe = () => {
             <div className={`shape2 ${isHovered ? 'hovered' : ''}`}
                 onMouseEnter={() => setIsHovered(true)}
             >
-                <div className="my-info"><span>By completing a bachelor's degree in CS, I have a strong foundation in programming,
+                <div className="my-info"><span style={{fontFamily:'sans-serif'}}>By completing a bachelor's degree in CS, I have a strong foundation in programming,
                     algorithms, and software design. With a passion for creating visually appealing and user-friendly web interfaces,
-                    I have focused my efforts on frontend development.
-                    As a computer science student, I have gained a broad range of skills and knowledge, from programming languages,to algorithms and data structures, computer architecture, database design and management, software engineering, web development, and more. While mastering these skills, I have also learned to prioritize performance and efficiency in my work. I understand the importance of optimizing code, minimizing resource usage, and designing systems that can handle large-scale data and user traffic. By balancing performance and aesthetics,
-                        I am able to develop innovative solutions that not only look good but also operate smoothly and reliably.</span></div>
+                    I have focused my efforts on frontend development. <br/>
+                    As a computer science student, I have gained a broad range of skills and knowledge, from programming languages,to algorithms and data structures,
+                    computer architecture, database design and management, software engineering, web development, 
+                    and more. While mastering these skills, I have also learned to prioritize performance and efficiency in my work. 
+                    I understand the importance of optimizing code, minimizing resource usage, and designing systems that can handle 
+                    large-scale data and user traffic. By balancing performance and aesthetics,
+                    I am able to develop innovative solutions that not only look good but also operate smoothly and reliably.</span></div>
             </div>
         </div>
     )
